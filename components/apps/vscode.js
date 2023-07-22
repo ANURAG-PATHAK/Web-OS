f13140a2fd4f1f299a9a02a4662a06f309d631ea
Anurag Pathak <pathakanurag605@gmail.com>
2023-07-22 22:00:57 +0530

    Update vscode.js

--- components/apps/vscode.js
+++ components/apps/vscode.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function VsCode() {
   return (
     <iframe
-      src="https://github.dev/ANURAG-PATHAK/my-portfolio/blob/main/.github/workflows/nextjs.yml"
+      src="https://github.dev/ANURAG-PATHAK/my-portfolio/blob/main"
       title="VsCode"
       className="h-full w-full bg-ub-cool-grey"
     ></iframe>
